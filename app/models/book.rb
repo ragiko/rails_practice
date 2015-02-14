ea3a7545a75ff2e8fc92dac16112100548bc3b2e
ragiko <ragiko.complexe@gmail.com>
2015-02-15 00:49:25 +0900

    validateのコールバックを作成

--- app/models/book.rb
+++ app/models/book.rb
@@ -11,10 +11,23 @@ class Book < ActiveRecord::Base
   validates :name, presence: true
   validates :name, length: { maximum: 15 } 
   validates :price, numericality: { greater_than_or_equal_to: 0 }
+  
   validate do |book|
     # エクササイズ嫌い validate
     if book.name.include?("exercise")
       book.errors[:name] << "I don't like exercise"
     end
+  end
+
+  before_validation do |book|
+    book.name = book.name.gsub(/Cat/) do |matched|
+      "lovely #{matched}"
+    end
+  end
+  
+  # オブジェクトを削除した後に呼ばれる
+  # log/development.logを見る
+  after_destroy do |book|
+    Rails.logger.info "Book is deleted: #{book.attributes.inspect}"
   end
 end
