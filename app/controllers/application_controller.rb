f2967ea8586d3a3886655f96c67e27c3a9710d7e
ragiko <ragiko.complexe@gmail.com>
2015-02-15 13:45:30 +0900

    モバイル端末などの切り替え処理を行う

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  # before_action :detect_mobile_variant
+  
+  private 
+  # variantは4.1から対応
+  # def detect_mobile_variant
+  #   request.variant = :mobile if request.user_agent =~ /iPhone/
+  # end
 end
