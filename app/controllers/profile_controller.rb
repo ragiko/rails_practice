7b2e4bce567bc23c2e7ece7865cf285d94d08038
ragiko <ragiko.complexe@gmail.com>
2015-02-15 12:35:17 +0900

    strong parameterの利用

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -6,5 +6,19 @@ class ProfileController < ApplicationController
   end
 
   def update
+    user = current_user
+    # params[:user] => {name: "bob", email: "a@example.com"}
+    user.update(user_params)
+  end
+  
+  private
+  # strong parametersによるバリデーション
+  def user_params
+    if current_user.admin?
+      # 受け取れるパラメーターはnameとemailのみ
+      params.require(:user).permit(:name, :email, :admin)
+    else
+      params.require(:user).permit(:name, :email)
+    end
   end
 end
